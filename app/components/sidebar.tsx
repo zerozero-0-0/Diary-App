6ad24f12f9a2e13f488cd724ceadbe35aab728af
zerozero-0-0 <seapara.zerozero.00@gmail.com>
2025-08-29 00:02:00 +0900

    fix: biomeが効くよう修正

--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -4,24 +4,25 @@ import { css } from "styled-system/css";
 export const Sidebar = () => {
 	const [isOpen, setIsOpen] = useState(false);
 
+    const handleClick = () => {
+        setIsOpen(!isOpen);
+    };
+
 	return (
 		<div>
-			{isOpen ? (
-				<></>
-			) : (
-				<button
-					type="button"
-					className={css({
-						position: "fixed",
-						height: "48px",
-						marginRight: 4,
-						zIndex: 1,
-						right: 0,
-					})}
-				>
-					<span className="material-icons">menu</span>
-				</button>
-			)}
+            <button
+                type="button"
+                className={css({
+                    position: "fixed",
+                    height: "48px",
+                    marginRight: 4,
+                    zIndex: 1,
+                    right: 0,
+                })}
+            >
+				<span className="material-icons" onClick={() => handleClick()}>menu</span>
+			</button>
+
 		</div>
 	);
 };
